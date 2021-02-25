19c20e1bbd84ae23f72e92f88a6aff09875e91b8
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2021-02-25 16:44:53 +0100

    Add Labeled-LDA wrapper

--- gensim/models/wrappers/__init__.py
+++ gensim/models/wrappers/__init__.py
@@ -3,6 +3,7 @@ This package contains wrappers for other topic modeling programs.
 """
 
 from .ldamallet import LdaMallet  # noqa:F401
+from .lldamallet import LLdaMallet  # noqa:F401
 from .dtmmodel import DtmModel  # noqa:F401
 from .ldavowpalwabbit import LdaVowpalWabbit  # noqa:F401
 from .wordrank import Wordrank  # noqa:F401
